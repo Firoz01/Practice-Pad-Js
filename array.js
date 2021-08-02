f6299d7a13e90915ed86252f3bd80cf10e274e93
Md Firoz Mia <67627130+Firoz01@users.noreply.github.com>
2021-08-03 03:34:36 +0600

    3.34am

---
+++ array.js
@@ -0,0 +1,9 @@
+const cars = ["Saab", "volvo", "BMW"];
+
+
+
+cars.sort();
+// console.log(cars.toString());  //convers into string
+
+
+console.log(cars.join(" * "));    // same as toString function but in additional we can specify the separator;
---
+++ number-methods.js
@@ -0,0 +1,13 @@
+// let x = 9.656;
+// x.toString();
+// console.log(x.toString());
+
+// let x = '657';
+// let y = Number(x);
+
+// console.log(y);
+
+
+
+
+
